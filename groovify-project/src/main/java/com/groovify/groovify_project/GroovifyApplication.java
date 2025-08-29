de5efe3ba0b401ec9a3dd2190fff01bac0e66d7c
Zack Gacnik <zgacnik@carroll.edu>
2025-08-29 10:54:15 -0600

    Test commit

---
+++ groovify-project/src/main/java/com/groovify/groovify_project/GroovifyApplication.java
@@ -0,0 +1,13 @@
+package com.groovify.groovify_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroovifyApplication.class, args);
+	}
+
+}
