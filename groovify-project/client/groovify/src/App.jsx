3c26cecb65c6394cf6b9bed306477a72cf5ad2e0
Zack Gacnik <zgacnik@carroll.edu>
2025-09-05 10:01:50 -0600

    Experimenting with React

---
+++ groovify-project/client/groovify/src/App.jsx
@@ -0,0 +1,29 @@
+import {useEffect, useState} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [title, setTitle] = useState('Default value')
+
+    useEffect(() => {
+        fetch("http://localhost:8080/").then(response=>response.text())
+            .then(text=>setTitle(text))
+            .catch(error=>console.log("Error fetching ", error))
+    }, []);
+
+  return (
+    <>
+        <head>
+            <title>Groovify</title>
+        </head>
+        <body>
+            <h1>{title}</h1>
+            <a href="" className="loginButton"><h2>Login</h2></a>
+            <a href="" className="loginButton"><h2>Register</h2></a>
+        </body>
+    </>
+  )
+}
+
+export default App
