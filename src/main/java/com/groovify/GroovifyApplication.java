9146c0dc2f03adbf5c42a4fec9903116220fae5d
Zack Gacnik <zgacnik@carroll.edu>
2025-09-11 15:45:47 -0600

    React impolementation put on main branch

---
+++ src/main/java/com/groovify/GroovifyApplication.java
@@ -0,0 +1,23 @@
+package com.groovify.groovify_project;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GroovifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroovifyApplication.class, args);
+    }
+
+    @CrossOrigin
+    @GetMapping("/")
+    public String index() {
+        return "Groovify";
+    }
+
+}
