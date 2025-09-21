38d2ce2ec5fa9503faed05e57d3f5dd3cc4a56a4
Zack Gacnik <zgacnik@carroll.edu>
2025-09-21 13:31:16 -0600

    Gantt chart added to documentation folder

--- src/main/java/com/groovify/GroovifyApplication.java
+++ src/main/java/com/groovify/GroovifyApplication.java
@@ -3,7 +3,6 @@ package com.groovify;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,16 +18,8 @@ public class GroovifyApplication {
         SpringApplication.run(GroovifyApplication.class, args);
     }
 
-    /**
-     * In order to connect the React front end with our Java backend, a CrossOrigin annotation
-     * is required. This enables CORS (Cross-Origin Resource Sharing) and allows a web application
-     * running on one domain (in this case our React front end running on localhost:5173) to connect
-     * to another domain (our Java backend running on localhost:8080).
-     */
-
-    @CrossOrigin
     @GetMapping("/")
-    public String index() {
+    public String landingPage() {
         return "Groovify";
     }
 
