be117a166f0d358fb4f10546cb1e29ad939b676a
Zack Gacnik <zgacnik@carroll.edu>
2025-09-25 14:21:28 -0600

    Login functionality working with success and failure pages

--- src/main/java/com/groovify/GroovifyApplication.java
+++ src/main/java/com/groovify/GroovifyApplication.java
@@ -1,26 +1,11 @@
 package com.groovify;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-
-/**
- * This class represents the web controller for our Groovify project. Since we are using a React front end,
- * we added a RestController annotation.
- */
 
 @SpringBootApplication
-@RestController
 public class GroovifyApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GroovifyApplication.class, args);
     }
-
-    @GetMapping("/")
-    public String landingPage() {
-        return "Groovify";
-    }
-
 }
---
+++ src/main/java/com/groovify/web/controller/LandingController.java
@@ -0,0 +1,12 @@
+package com.groovify.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LandingController {
+    @GetMapping("/")
+    public String landingPage() {
+        return "landingPage"; // looks for landingPage.html in /templates
+    }
+}
