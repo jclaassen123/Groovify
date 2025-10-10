304b2fd7c182e87839f2aa4ffe3f7b2317478e0a
Nevin Fullerton <nfullerton@carroll.edu>
2025-10-10 00:31:59 -0600

    Implemented pause play, progress bar that allows user to seek in song, and volume slider.

--- src/main/resources/static/js/mediaplayer.js
+++ src/main/resources/static/js/mediaplayer.js
@@ -1,17 +1,69 @@
 const playButton = document.getElementById("playPause");
 const lastSongButton = document.getElementById("lastSong");
 const nextSongButton = document.getElementById("nextSong");
+const progressBar = document.getElementById("progress");
+const volumeBar = document.getElementById("volume");
+const audioPlayer = document.getElementById("player");
 
-var currentSong = null;
+let currentSong = null;
+const songFolderPath = "/songs/";
 
-const listedSongs = document.querySelectorAll("#songHolder")
+const listedSongs = document.querySelectorAll("#songHolder .song");
 
 listedSongs.forEach(song => {
     song.addEventListener('click', () => {
-        const newSong = button.getAttribute("data");
+        const newSong = song.getAttribute("data-filename");
         if (newSong !== currentSong) {
-            currentSong = newSong
+            currentSong = newSong;
+            audioPlayer.src = songFolderPath + encodeURIComponent(currentSong); // encode filename to support more browsers
+            playSong();
+            playButton.disabled = false;
 
+        } else {
+            audioPlayer.currentTime = 0;
         }
-    })
-})
+    });
+});
+
+playButton.addEventListener('click', () => {
+    if (audioPlayer.paused) {
+        playSong();
+    } else {
+        pauseSong();
+    }
+});
+
+audioPlayer.addEventListener('loadedmetadata', () => {
+    progressBar.max = audioPlayer.duration;
+});
+
+audioPlayer.addEventListener('timeupdate', () => {
+    if (audioPlayer.duration) {
+        progressBar.value = audioPlayer.currentTime;
+    }
+});
+
+audioPlayer.addEventListener('ended', () => {
+    playButton.disabled = true;
+    playButton.textContent = "Play";
+});
+
+progressBar.addEventListener('input', () => {
+    if (audioPlayer.duration) {
+        audioPlayer.currentTime = progressBar.value;
+    }
+});
+
+volumeBar.addEventListener('input', () => {
+   audioPlayer.volume = volumeBar.value;
+});
+
+function playSong() {
+    audioPlayer.play();
+    playButton.textContent = "Pause";
+}
+
+function pauseSong() {
+    audioPlayer.pause();
+    playButton.textContent = "Play";
+}
