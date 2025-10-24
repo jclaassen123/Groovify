c08f68d6253bb6d2eade513e2e5df4939f8f781e
Zack Gacnik <zgacnik@carroll.edu>
2025-10-24 07:50:33 -0600

    Login and landing page unit tests added

--- src/test/java/com/groovify/GroovifyApplicationTests.java
+++ src/test/java/com/groovify/GroovifyApplicationTests.java
@@ -1,27 +1,22 @@
 package com.groovify;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import com.groovify.web.controller.LandingController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 
-/**
- * This class is used for Unit Testing on the Login / Register page. If the back end
- * is down or not working properly and doesn't print "Groovify" to that page, an error
- * is thrown. Otherwise, the build is completed successfully.
- */
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
-@WebMvcTest
+@SpringBootTest      // Loads full application context
+@AutoConfigureMockMvc // Enables MockMvc without WebMvcTest
 class GroovifyApplicationTests {
 
     @Autowired
-    private MockMvc mockMvc;
+    private MockMvc mockMvc;  // No MockBean needed
 
     @Test
     void indexTest() throws Exception {
