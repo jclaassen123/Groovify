fdb38349a03d2bd52f31006e9f95b9cf5f1a5970
Nevin Fullerton <nfullerton@carroll.edu>
2025-11-06 20:40:11 -0700

    user can create playlist

--- src/main/resources/static/js/playlists.js
+++ src/main/resources/static/js/playlists.js
@@ -1,1 +1,18 @@
 // This is a single-line comment
+    // Get modal and button elements
+    const modal = document.getElementById("playlistModal");
+    const btn = document.getElementById("createPlaylistBtn");
+    const closeBtn = modal.querySelector(".close");
+
+    // Open modal when button is clicked
+    btn.onclick = () => modal.style.display = "block";
+
+    // Close modal when clicking the 'x'
+    closeBtn.onclick = () => modal.style.display = "none";
+
+    // Close modal if clicking outside the modal content
+    window.onclick = (event) => {
+        if (event.target === modal) {
+        modal.style.display = "none";
+        }
+    }
